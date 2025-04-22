191637563306853f0cfea2ce705e86fc66617207
Jack <jfrathbone@gmail.com>
2025-04-22 13:47:53 +0200

    Can place and remove pipes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private int _startingPipes = 25;
+
 
     [Header("References")]
-    [SerializeField] GameObject _waterPipePrefab;
+    [SerializeField] private GameObject _waterPipePrefab;
+    private GameObject _placementPreview;
 
     private Rigidbody2D _rigidbody;
     private SpriteRenderer _spriteRenderer;
 
+    private Tilemap _tileMap;
+
     [Header("Data")]
+    private int _currentPipes;
+
     private bool _facingLeft = false;
-    private bool _freezeMovement = false;
+    private bool _freezeInput = false;
 
     //For movement
     private float _horizontalInput;
@@ -22,21 +30,51 @@ public class PlayerController : MonoBehaviour
     private Vector2 _newPosition;
     private Vector2 _mouseWorldPosition;
 
+    //For pipe placement
+    private Vector3Int _gridPosition;
+    private TileBase _hoveredTile;
+    private Vector2 _cellCenterWorldPosition;
+    private RaycastHit2D _hit;
+
     private void Start()
     {
+        _currentPipes = _startingPipes;
+
+        _placementPreview = transform.Find("PlacePreview").gameObject;
+        _placementPreview.SetActive(false);
+
         _rigidbody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _tileMap = FindObjectOfType<Tilemap>();
     }
 
     private void Update()
     {
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _verticalInput = Input.GetAxis("Vertical");
+        if (!_freezeInput)
+        {
+            _horizontalInput = Input.GetAxis("Horizontal");
+            _verticalInput = Input.GetAxis("Vertical");
+
+            _mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            CheckPlacement();
+
+            if (_placementPreview.activeInHierarchy && Input.GetButton("Fire1"))
+            {
+                PlacePipe();
+            }
+
+            if (Input.GetButton("Fire2"))
+            {
+                RemovePipe();
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        if (!_freezeMovement)
+        if (!_freezeInput)
         {
             CheckMovement();
             CheckLookDirection();
@@ -54,8 +92,6 @@ public class PlayerController : MonoBehaviour
 
     private void CheckLookDirection()
     {
-        _mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
         if (_mouseWorldPosition.x > transform.position.x)
         {
             _spriteRenderer.flipX = false;
@@ -67,6 +103,55 @@ public class PlayerController : MonoBehaviour
         else
         {
             return;
+        }
+    }
+
+    private void CheckPlacement()
+    {
+        _gridPosition = _tileMap.WorldToCell(_mouseWorldPosition);
+        _hoveredTile = _tileMap.GetTile(_gridPosition);
+        _cellCenterWorldPosition = _tileMap.GetCellCenterWorld(_gridPosition);
+
+        if (_hoveredTile == null)
+        {
+            _placementPreview.SetActive(false);
+            return;
+        }
+        else
+        {
+            _hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (_hit.collider == null)
+            {
+                _placementPreview.SetActive(true);
+                _placementPreview.transform.position = _cellCenterWorldPosition;
+            }
+            else
+            {
+                _placementPreview.SetActive(false);
+            }
+        }
+    }
+
+    private void PlacePipe()
+    {
+        if (_currentPipes > 0)
+        {
+            Instantiate(_waterPipePrefab, _tileMap.GetCellCenterLocal(_gridPosition), Quaternion.identity);
+            _currentPipes--;
+        }
+    }
+
+    private void RemovePipe()
+    {
+        _hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (_hit.collider != null)
+        {
+            if (_hit.collider.CompareTag("Pipe"))
+            {
+                Destroy(_hit.collider.gameObject);
+                _currentPipes++;
+            }
         }
     }
 }
