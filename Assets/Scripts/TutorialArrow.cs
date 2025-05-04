b4f781322706165b3bf28f487f17e58f961e032c
Jack R <80467074+JackFrathbone@users.noreply.github.com>
2025-05-04 13:54:10 +0200

    tee

--- Assets/Scripts/TutorialArrow.cs
+++ Assets/Scripts/TutorialArrow.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TutorialArrow : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] DialogueObject _tutorialDialogue;
 
+    [SerializeField] private UnityEvent _RunOnDestroyEvent;
+
     public DialogueObject GetTutorialDialogue()
     {
         return _tutorialDialogue;
+    }
+
+    private void OnDestroy()
+    {
+        _RunOnDestroyEvent.Invoke();
     }
 }
